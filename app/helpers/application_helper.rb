0075c4d7b956573b116e7a48f6e6ed18a6d78286
David Montesdeoca <info@davidmontesdeoca.net>
2018-04-14 14:18:22 +0200

    Use helpers to build configuration for meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,45 @@ module ApplicationHelper
   def current_locale?(lang)
     I18n.locale == lang.to_sym
   end
+
+  # Builds multilingual links for search engines
+  #
+  # @return [Hash] multilingual links for search engines
+  def meta_tags_alternate_config
+    I18n.available_locales.each_with_object({}) do |locale, memo|
+      memo[locale] = root_url(locale: locale)
+    end
+  end
+
+  # Builds favicons for search engines
+  #
+  # Assets pipeline is skipped, so images are in public folder
+  #
+  # @return [Array<Hash>] favicons for search engines
+  def meta_tags_icon_config
+    [
+      {
+        href: asset_path('favicon-16x16.png', skip_pipeline: true),
+        sizes: '16x16',
+        type: 'image/png'
+      },
+      {
+        href: asset_path('favicon-32x32.png', skip_pipeline: true),
+        sizes: '32x32',
+        type: 'image/png'
+      },
+      {
+        href: asset_path('apple-touch-icon-144x144.png', skip_pipeline: true),
+        rel: 'apple-touch-icon-precomposed',
+        sizes: '144x144',
+        type: 'image/png'
+      },
+      {
+        href: asset_path('apple-touch-icon-152x152.png', skip_pipeline: true),
+        rel: 'apple-touch-icon-precomposed',
+        sizes: '152x152',
+        type: 'image/png'
+      }
+    ]
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 RSpec.describe ApplicationHelper, type: :helper do
   describe 'current_locale?(lang)' do
     before do
-      @orig_locale = I18n.locale
+      @original_locale = I18n.locale
       I18n.locale = :en
     end
 
     after do
-      I18n.locale = @orig_locale
+      I18n.locale = @original_locale
     end
 
     it 'is true when matches with given locale (as symbol)' do
@@ -21,6 +21,35 @@ RSpec.describe ApplicationHelper, type: :helper do
 
     it 'is false when does not match with given locale' do
       expect(helper.current_locale?(:es)).to be_falsey
+    end
+  end
+
+  describe 'meta_tags_alternate_config' do
+    before do
+      @available_locales = I18n.available_locales
+      I18n.available_locales = [:en, :es]
+    end
+
+    after do
+      I18n.available_locales = @available_locales
+    end
+
+    it 'returns multilingual links for search engines' do
+      expect(helper.meta_tags_alternate_config).to eq({
+        en: root_url(locale: :en),
+        es: root_url(locale: :es)
+      })
+    end
+  end
+
+  describe 'meta_tags_icon_config' do
+    it 'returns favicons for search engines' do
+      expect(helper.meta_tags_icon_config).to eq([
+        { href: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+        { href: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+        { href: '/apple-touch-icon-144x144.png', rel: 'apple-touch-icon-precomposed', sizes: '144x144', type: 'image/png' },
+        { href: '/apple-touch-icon-152x152.png', rel: 'apple-touch-icon-precomposed', sizes: '152x152', type: 'image/png' }
+      ])
     end
   end
 end
